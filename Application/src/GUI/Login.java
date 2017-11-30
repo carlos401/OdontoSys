baa6e00a52745250d70ab396eb30588a781d960e
Jose Alfaro <josealfarow@gmail.com>
2017-11-30 04:31:10 -0600

    LOGIN form advance

--- Application/src/GUI/Login.java
+++ Application/src/GUI/Login.java
@@ -1,4 +1,20 @@
 package GUI;
 
+import javax.swing.*;
+
 public class Login {
+
+
+    private JPanel LOGINPanel;
+    private JLabel ToothLogo;
+    private JButton LOGINButton;
+    private JLabel UserLogo;
+    private JLabel PassLogo;
+    private JLabel WelcomeLabel;
+    private JTextField userNameTextField;
+    private JPasswordField PassField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
