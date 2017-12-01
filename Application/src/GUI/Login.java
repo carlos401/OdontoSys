730f39ed7dd66c0200140c58a4a2df4022fc6138
carlos401 <carlos.delgadorojas@ucr.ac.cr>
2017-11-30 18:37:49 -0600

    Model applied

---
+++ Application/src/GUI/Login.java
@@ -0,0 +1,12 @@
+/**
+ * UNIVERSIDAD DE COSTA RICA
+ * ESCUELA DE CIENCIAS DE LA COMPUTACIÓN E INFORMÁTICA
+ * CI1312 - BASES DE DATOS I
+ * @version 1.0
+ * @author Carlos Delgado Rojas
+ */
+
+package GUI;
+
+public class Login {
+}
